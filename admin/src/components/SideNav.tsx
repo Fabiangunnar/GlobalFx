aa1d7d7d42cc693cc7ec53a25ab9b0f639d74665
fab <fabiangunnar9@gmail.com>
2023-06-14 08:40:20 +0200

    New Update

--- admin/src/components/SideNav.tsx
+++ admin/src/components/SideNav.tsx
@@ -1,46 +1,69 @@
-import React, { useEffect } from "react";
-import { NavTypes, getIconComponent } from "../data/sidenav";
+import React, {useEffect} from "react";
+import {NavTypes, getIconComponent} from "../data/sidenav";
 import Link from "next/link";
 import styles from "@/styles/SideNav.module.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { closeNav, setCurrentPage, setNavLink } from "@/redux-actions/navSlice";
+import {useSelector, useDispatch} from "react-redux";
+import {closeNav, setCurrentPage, setNavLink} from "@/redux-actions/navSlice";
 
 type Props = {};
 
 const SideNav = (props: Props) => {
-	const { navData } = useSelector((state: any) => state.nav);
-	const dispatch = useDispatch();
-	const handleChangeNav = (link: NavTypes) => {
-		dispatch(setCurrentPage(link.link));
-		dispatch(setNavLink(link));
-		dispatch(closeNav());
-	};
+  const {navData} = useSelector((state: any) => state.nav);
+  const dispatch = useDispatch();
+  const handleChangeNav = (link: NavTypes) => {
+    dispatch(setCurrentPage(link.link));
+    dispatch(setNavLink(link));
+    dispatch(closeNav());
+  };
 
-	return (
-		<div className={`${styles.sidenav_block}`}>
-			<h1>Admin</h1>
-			<div className={`${styles.sidenav_items}`}>
-				<h5>Menus</h5>
-				<ul className={`${styles.nav_links}`}>
-					{navData.map((item: NavTypes) => {
-						const icon = getIconComponent(item.icon);
-						return (
-							<li
-								className={`${styles.nav_link} ${
-									item?.state ? styles.active : ""
-								}
+  return (
+    <div className={`${styles.sidenav_block}`}>
+      <h1>Admin</h1>
+      <div className={`${styles.sidenav_items}`}>
+        <h5>Menus</h5>
+        <ul className={`${styles.nav_links}`}>
+          {navData.map((item: NavTypes) => {
+            const icon = getIconComponent(item.icon);
+            if (item.livelink) {
+              return (
+                <Link
+                  href={`${item.livelink}`}
+                  target="_blank"
+                  style={{
+                    width: "100%",
+                  }}
+                >
+                  <li
+                    className={`${styles.nav_link} ${
+                      item?.state ? styles.active : ""
+                    }
+                                    `}
+                    key={item?.id}
+                  >
+                    <span>{icon}</span>
+                    <span>{item?.desc} </span>
+                  </li>
+                </Link>
+              );
+            }
+            return (
+              <li
+                className={`${styles.nav_link} ${
+                  item?.state ? styles.active : ""
+                }
                             `}
-								key={item?.id}
-								onClick={() => handleChangeNav(item)}>
-								<span>{icon}</span>
-								<span>{item?.desc} </span>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</div>
-	);
+                key={item?.id}
+                onClick={() => handleChangeNav(item)}
+              >
+                <span>{icon}</span>
+                <span>{item?.desc} </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SideNav;

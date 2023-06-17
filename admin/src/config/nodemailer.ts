8393940e5af0e7e51dbf38f2d3b223f285dab1fa
Fabi <fabiangunnar9@gmail.com>
2023-06-17 21:53:08 +0200

    Update nodemailer.ts

--- admin/src/config/nodemailer.ts
+++ admin/src/config/nodemailer.ts
@@ -1,6 +1,7 @@
 // create reusable transporter object using the default SMTP transport
 import nodemailer from "nodemailer";
 import sendGridTransport from "nodemailer-sendgrid-transport";
+
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 console.log(email, pass);

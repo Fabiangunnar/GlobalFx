fcc5800035dc8e774cf9bf690da08cbc31afa703
Fab <fabiangunnar9@gmail.com>
2024-05-27 19:22:41 +0100

    Update Mainlayout.tsx

--- client/components/layout/Mainlayout.tsx
+++ client/components/layout/Mainlayout.tsx
@@ -58,6 +58,14 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     };
   }, []);
   useEffect(() => {
+    // This will run only once when the app loads
+    const script = document.createElement("script");
+    script.src = "//code.tidio.co/your-unique-code.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  useEffect(() => {
     if (pathname !== "/") return;
     const timer = setTimeout(() => {
       setShowPrompt(true);
@@ -97,7 +105,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo2-transparent.png" />
       </Head> */}
-
+      <script
+        src="//code.tidio.co/nmy1l0dbc5rnh69waecfutgc50nqexpj.js"
+        async
+      ></script>{" "}
       <NotificationPopup />
       <div className={`app-block  ${styles.home_app_block}`}>
         <header
